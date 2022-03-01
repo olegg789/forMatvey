39a026832a045b6cf56699d7eabbab3a93074c7b
Oleg <86119518+olegg789@users.noreply.github.com>
2022-03-01 08:33:49 +0300

    Update allNotes.js

--- src/js/panels/home/allNotes.js
+++ src/js/panels/home/allNotes.js
@@ -5,7 +5,7 @@ import {
     Div,
     Footer,
     FormItem,
-    FormLayout, Snackbar
+    FormLayout, Snackbar, Header
 } from "@vkontakte/vkui";
 
 import {
@@ -92,7 +92,7 @@ function AllNotes({router, allNotes, isDesktop, editNote, openSnackbar, getNotes
                             <FormLayout>
                                 <FormItem
                                     top={
-                                        el.name
+                                        <Header mode='secondary'>{el.name}</Header>
                                     }
                                     bottom={
                                         <>
