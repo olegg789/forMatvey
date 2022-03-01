c9ba99e0f784e82d015a18fa92f4f8d5a13e40d1
Oleg <86119518+olegg789@users.noreply.github.com>
2022-03-01 08:36:47 +0300

    Update majorNotes.js

--- src/js/panels/home/majorNotes.js
+++ src/js/panels/home/majorNotes.js
@@ -5,7 +5,7 @@ import {
     Div,
     Footer,
     FormItem,
-    FormLayout, Snackbar
+    FormLayout, Snackbar, Header
 } from "@vkontakte/vkui";
 
 import {
@@ -92,7 +92,7 @@ function MajorNotes({majorNotes, getMajorNotes, router, isDesktop, editNote, ope
                             <FormLayout>
                                 <FormItem
                                     top={
-                                        el.name
+                                        <Header mode='secondary'>{el.name}</Header>
                                     }
                                     bottom={
                                         <>
