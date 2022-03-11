10c1378346e5d45b2e0b98e5695fa4c4b8adf1c3
verTuego <82771385+ver-tuego@users.noreply.github.com>
2022-03-11 22:54:12 +0300

    теперь сервис общается с помощью post-запросов
    а также пофиксил 3 бага:
    https://vk.com/bug351343; https://vk.com/bug351290; https://vk.com/bug351280

--- api.py
+++ api.py
@@ -10,6 +10,7 @@ api = Api(app)
 CORS(app)
 
 import re
+import time
 import actionDB
 
 client_secret = 'cmlhxM5rbfQ6ExlzAxW7'
@@ -40,60 +41,76 @@ def is_valid(query: dict, secret: str) -> bool:
     fixed_hash = hash_code.replace('+', '-').replace('/', '_')
     return query.get("sign") == fixed_hash
 
+timeUsers = dict()
 
 class notes(Resource):
 
-        def get(self):
-                if len(request.args.to_dict()) == 0:
+        def post(self):
+                if len(request.get_json()) == 0:
                         return {"error": True, "code": 5, "messsage": 'Incorrectly passed parameters'}, 400
-                if request.args.to_dict().get('method') == None or request.args.to_dict().get('access_token') == None:
+                if request.get_json().get('method') == None or request.get_json().get('access_token') == None:
                     return {"error": True, "code": 5, "messsage": 'Incorrectly passed parameters'}, 400
                 query_params = dict(
                     parse_qsl(
-                        urlparse(f"https://example.com/?{request.args.to_dict().get('access_token').replace('/', '&')}").query,
+                        urlparse(f"https://example.com/?{request.get_json().get('access_token')}").query,
                         keep_blank_values=True
                     )
                 )
                 status = is_valid(query=query_params, secret=client_secret)
                 if status:
-                    if request.args.to_dict().get('method') == 'notes.getMyNotes':
-                        resultData = actionDB.getData(query_params['vk_user_id'])
+                    if request.get_json().get('method') == 'notes.getMyNotes':
+                        resultData = actionDB.getData(query_params['vk_user_id'], True, None)
                         return {"count": resultData[0], "items": resultData[1]}, 200
 
-                    if request.args.to_dict().get('method') == 'notes.createNote' or request.args.to_dict().get('method') == 'notes.editNote':
-                        params = ['status', 'priority', 'name','value']
-                        for param in range(4):
-                                if request.args.to_dict().get(params[param]) == None or request.args.to_dict().get(params[param]) == '':
-                                        return {"error": True, "code": f'1{param}', "messsage": 'One of the parameters is invalid'}, 400
-                        for param in range(2):
-                                result = re.match('^[0-9]+$', request.args.to_dict().get(params[param]))
+                    if request.get_json().get('method') == 'notes.createNote' or request.get_json().get('method') == 'notes.editNote':
+                        if timeUsers.get(query_params['vk_user_id']) is None: timeUsers[query_params['vk_user_id']] = time.time() - 1
+
+                        if (time.time() - timeUsers[query_params['vk_user_id']]) > 0 or (time.time() - timeUsers[query_params['vk_user_id']] - 4) > 0:
+                                params = ['status', 'priority', 'name','value']
+                                for param in range(4):
+                                        if request.get_json().get(params[param]) == None or not str(request.get_json().get(params[param])).strip():
+                                                return {"error": True, "code": f'1{param}', "messsage": 'One of the parameters is invalid'}, 400
+                                        if len(request.get_json().get(params[param])) > 300:
+                                                return {"error": True, "code": f'1{param}', "messsage": 'One of the parameters is invalid'}, 400
+                                for param in range(2):
+                                        result = re.match('^[0-9]+$', str(request.get_json().get(params[param])))
+                                        if result == None:
+                                                return {"error": True, "code": f'1{param}', "messsage": 'One of the parameters is invalid'}, 400
+                                        if request.get_json().get(params[param]) < 0 or request.get_json().get(params[param]) > 3:
+                                                return {"error": True, "code": f'1{param}', "messsage": 'One of the parameters is invalid'}, 400
+                                if request.get_json().get('method') == 'notes.createNote':
+                                        if (time.time() - timeUsers[query_params['vk_user_id']]) > 0:
+                                                timeUsers[query_params['vk_user_id']] = time.time() + 5
+                                                resultData = actionDB.createNote(query_params['vk_user_id'], request.get_json().get('name'), request.get_json().get('value'), request.get_json().get('priority'), request.get_json().get('status'))
+                                                return resultData, 200
+                                        else:
+                                                return {"error": True, "code": '7', "message": 'today no flood'}
+
+                                if request.get_json().get('noteId') == None or not str(request.get_json().get('noteId')).strip():
+                                        return {"error": True, "code": 14, "messsage": 'One of the parameters is invalid'}, 400
+                                result = re.match('^[0-9]+$', str(request.get_json().get('noteId')))
                                 if result == None:
-                                        return {"error": True, "code": f'1{param}', "messsage": 'One of the parameters is invalid'}, 400
-                                if int(request.args.to_dict().get(params[param])) < 0 or int(request.args.to_dict().get(params[param])) > 3:
-                                        return {"error": True, "code": f'1{param}', "messsage": 'One of the parameters is invalid'}, 400
-                        if request.args.to_dict().get('method') == 'notes.createNote':
-                                resultData = actionDB.createNote(query_params['vk_user_id'], request.args.to_dict().get('name').replace('¦', '&'), request.args.to_dict().get('value').replace('¦', '&'), request.args.to_dict().get('priority'), request.args.to_dict().get('status'))
-                                return {"message": resultData}, 200
-
-                        if request.args.to_dict().get('noteId') == None or request.args.to_dict().get('noteId') == '':
                                         return {"error": True, "code": 14, "messsage": 'One of the parameters is invalid'}, 400
-                        result = re.match('^[0-9]+$', request.args.to_dict().get('noteId'))
-                        if result == None:
-                                        return {"error": True, "code": 14, "messsage": 'One of the parameters is invalid'}, 400
-                        if request.args.to_dict().get('method') == 'notes.editNote':
-                                resultData =  actionDB.editNote(int(query_params['vk_user_id']), request.args.to_dict().get('noteId'), request.args.to_dict().get('name').replace('¦', '&'), request.args.to_dict().get('value').replace('¦', '&'), request.args.to_dict().get('priority'), request.args.to_dict().get('status'))
-                                return resultData
+                                if request.get_json().get('method') == 'notes.editNote':
+                                        if (time.time() - timeUsers[query_params['vk_user_id']]) > 0:
+                                                timeUsers[query_params['vk_user_id']] = time.time() + 1
+                                                resultData =  actionDB.editNote(int(query_params['vk_user_id']), request.get_json().get('noteId'), request.get_json().get('name'), request.get_json().get('value'), request.get_json().get('priority'), request.get_json().get('status'))
+                                                return resultData
+                                        else:
+                                                return {"error": True, "code": '7', "message": 'today no flood'}, 418
+                        else:
+                                return {"error": True, "code": '7', "message": 'today no flood'}, 418
 
-                    if request.args.to_dict().get('method') == 'notes.deleteNote':
-                        if request.args.to_dict().get('noteId') == None or request.args.to_dict().get('noteId') == '':
+                    if request.get_json().get('method') == 'notes.deleteNote':
+                        if request.get_json().get('noteId') == None or not str(request.get_json().get('noteId')).strip():
                                 return {"error": True, "messsage": 'Invalid passed noteId'}, 400
-                        noteId = re.match('^[0-9]+$', request.args.to_dict().get('noteId'))
+                        noteId = re.match('^[0-9]+$', str(request.get_json().get('noteId')))
                         if noteId == None:
                                 return {"error": True, "messsage": 'Invalid passed noteId'}, 400
                         else:
-                                resultData = actionDB.deleteNote(int(query_params['vk_user_id']), int(request.args.to_dict().get('noteId')), False)
+                                resultData = actionDB.deleteNote(int(query_params['vk_user_id']), int(request.get_json().get('noteId')), False)
                                 return resultData
-                    if request.args.to_dict().get('method') == 'notes.deleteAllNotes':
+                    if request.get_json().get('method') == 'notes.deleteAllNotes':
                         resultData = actionDB.deleteNote(int(query_params['vk_user_id']), None, True)
                         return resultData
                     else:
